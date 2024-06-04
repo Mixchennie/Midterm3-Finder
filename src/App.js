2147854ec14d6bd5d8e56ada88b86db5d722d5b6
Mixchennie0505 <mixchennie@gmail.com>
2024-06-04 11:50:03 +0700

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from './components/users/Search.js';
-
-function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-        console.log("GitHub Users:", response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-
+import Search from "./components/users/Search";
+const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-        <Search />
-      </div>
-      <Users users={users} />
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
-}
-
+};
 export default App;
